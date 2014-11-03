49d4cc0c7457d44dcd37578762bb1762bfc54d96
KaneZLK <zhulikai@hotmail.com>
2014-11-03 14:57:10 +0800

    upgrade CSS
    upgrade all css and the banner of news page

--- src/js/resetSize.js
+++ src/js/resetSize.js
@@ -5,9 +5,11 @@
         var w = html.clientWidth;
         var h = html.clientHeight;
         var baseSize = 12;
+        //var baseScale = 1.15;
+        var baseScale = 1;
         var baseWidth = 1700;
-        var scale = w / baseWidth;
-        var fontSize = Math.floor(scale * baseSize);
+        var scale = w / baseWidth / baseScale;
+        var fontSize = scale * baseSize;
         fontSize = fontSize < baseSize ? baseSize : fontSize;
         if (fontSize === html[field]) {
             return false;
